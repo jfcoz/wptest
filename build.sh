590630d75d8094dd9f8fb75b9be5cadd0f236575
Julien Francoz <julien@les-tilleuls.coop>
2022-12-07 13:54:00 +0100

    re-enable ilab-media-cloud plugin

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# build is done via buildx and needs quemu-user-static
 set -e
 repo=jfcoz/wptest
 tag=$(date +%Y%m%d-%H%M)
 echo building tag $tag
 docker buildx build \
+	--pull \
 	--platform linux/arm64,linux/amd64 \
 	--cache-from $repo:cache \
 	--cache-to $repo:cache \
--- wp-config.php
+++ wp-config.php
@@ -58,29 +58,44 @@ define( 'AUTOMATIC_UPDATER_DISABLED', true );
 // wordpress path
 //define( 'ABSPATH', __DIR__ . '/wp/' );
 
-//  wp redis plugin
-define( 'WP_REDIS_HOST', getenv("REDIS_HOST") );
-define( 'WP_REDIS_PORT', getenv("REDIS_PORT") );
-define( 'WP_REDIS_TIMEOUT', 1 );
-define( 'WP_REDIS_READ_TIMEOUT', 1 );
-define( 'WP_REDIS_DATABASE', getenv("REDIS_DATABASE") );
-// supported clients: `phpredis`, `credis`, `predis` and `hhvm`
-define( 'WP_REDIS_CLIENT', 'phpredis' );
-// automatically delete cache keys after 7 days
-// define( 'WP_REDIS_MAXTTL', 60 * 60 * 24 * 7 );
-// bypass the object cache, useful for debugging
-// define( 'WP_REDIS_DISABLED', true );
+////  wp redis plugin
+//define('WP_REDIS_HOST', getenv("REDIS_HOST") );
+//define('WP_REDIS_PORT', getenv("REDIS_PORT") );
+//define('WP_REDIS_TIMEOUT', 1 );
+//define('WP_REDIS_READ_TIMEOUT', 1 );
+//define('WP_REDIS_DATABASE', getenv("REDIS_DATABASE") );
+//// supported clients: `phpredis`, `credis`, `predis` and `hhvm`
+//define( 'WP_REDIS_CLIENT', 'phpredis' );
+//// automatically delete cache keys after 7 days
+//// define( 'WP_REDIS_MAXTTL', 60 * 60 * 24 * 7 );
+//// bypass the object cache, useful for debugging
+//// define( 'WP_REDIS_DISABLED', true );
 
 
 // humanmade/S3-Uploads
-define( 'S3_UPLOADS_BUCKET', getenv("S3_BUCKET") );
-define( 'S3_UPLOADS_REGION', getenv("S3_REGION") );
-define( 'S3_UPLOADS_ENDPOINT', getenv("S3_ENDPOINT") );
-define( 'S3_UPLOADS_KEY', getenv("S3_ACCESS_KEY"));
-define( 'S3_UPLOADS_SECRET', getenv("S3_SECRET"));
-define( 'S3_UPLOADS_BUCKET_URL', getenv("S3_ENDPOINT")."/".getenv("S3_BUCKET") );
+define('S3_UPLOADS_BUCKET', getenv("S3_BUCKET") );
+define('S3_UPLOADS_REGION', getenv("S3_REGION") );
+define('S3_UPLOADS_ENDPOINT', getenv("S3_ENDPOINT") );
+define('S3_UPLOADS_KEY', getenv("S3_ACCESS_KEY"));
+define('S3_UPLOADS_SECRET', getenv("S3_SECRET"));
+define('S3_UPLOADS_BUCKET_URL', getenv("S3_ENDPOINT")."/".getenv("S3_BUCKET") );
 // give backend type to mu-plugin/s3-endpoint.php
-define( 'S3_PROVISIONNER', getenv("S3_PROVISIONNER") );
+define('S3_PROVISIONNER', getenv("S3_PROVISIONNER") );
+
+// ilba media cloud
+if (getenv('S3_ENDPOINT')) {
+  define('MCLOUD_STORAGE_PROVIDER', 'other-s3');
+} else {
+  define('MCLOUD_STORAGE_PROVIDER', getenv('S3_PROVIDER'));
+}
+define('MCLOUD_STORAGE_S3_BUCKET', getenv("S3_BUCKET") );
+define('MCLOUD_STORAGE_S3_REGION', getenv("S3_REGION") );
+define('MCLOUD_STORAGE_S3_ENDPOINT', getenv("S3_ENDPOINT") );
+define('MCLOUD_STORAGE_S3_ACCESS_KEY', getenv("S3_ACCESS_KEY"));
+define('MCLOUD_STORAGE_S3_SECRET', getenv("S3_SECRET"));
+define('MCLOUD_STORAGE_S3_USE_PATH_STYLE_ENDPOINT', getenv("S3_USE_PATH_STYLE") );
+
+
 
 require_once ABSPATH . 'wp-settings.php';
 
